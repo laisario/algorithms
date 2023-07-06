fa2d8486e39c4535c9a9135a311fc5ea6135e713
Laisa Rio Verde <laisa.rioverde@gmail.com>
2023-07-06 18:50:56 -0300

    arrumei erros de lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -7,12 +7,5 @@ def study_schedule(permanence_period, target_time):
         if type(student[0]) != int or type(student[1]) != int:
             return None
         if student[0] <= target_time <= student[1]:
-            count += 1 
+            count += 1
     return count
-             
-    raise NotImplementedError
-
-print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 5))
-
-# student[0] <= target_time and student[1] >= target_time:
-# count = [*range(student[0], student[1])]
