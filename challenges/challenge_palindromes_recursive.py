196639d536de933d93f0d6e9921ae821310176cb
Laisa Rio Verde <laisa.rioverde@gmail.com>
2023-07-11 15:28:07 -0300

    requisito de teste

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
+    if len(word) == 0:
+        return False
+    not_reverse = word
+    word[::-1]
+    if not_reverse == word:
+        return True
+    return False
     """Faça o código aqui."""
     raise NotImplementedError
+
+
+print(is_palindrome_recursive('aggua', 1, 2))
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    'Test function encrypt_message'
+    assert encrypt_message('test', 2) == 'ts_et'
+    assert encrypt_message('test', 3) == 'set_t'
+    assert encrypt_message('test', 6) == 'tset'
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(4, 2)
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message('aaa', 'bbb')
