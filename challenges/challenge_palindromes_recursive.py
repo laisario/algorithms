7a56485dbfefb31f508bd3f6064e41e31fc4aa92
Laisa Rio Verde <laisa.rioverde@gmail.com>
2023-07-13 15:05:11 -0300

    corrigindo erros de lint

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,7 +1,7 @@
 def is_palindrome_recursive(word, low_index, high_index):
     if not word:
         return False
-    if  high_index == low_index:
+    if high_index == low_index:
         return True
     if (high_index - low_index) == 1 and word[low_index] == word[high_index]:
         return True
@@ -9,5 +9,3 @@ def is_palindrome_recursive(word, low_index, high_index):
         return is_palindrome_recursive(word, low_index + 1, high_index - 1)
     else:
         return False
-
-
