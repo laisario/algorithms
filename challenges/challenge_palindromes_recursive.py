cf7923de6fd3c4d960abcff07e64cf54efa167b7
Laisa Rio Verde <laisa.rioverde@gmail.com>
2023-07-13 14:59:47 -0300

    requisito palindromo recursivo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,6 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    if len(word) == 0 or word == '':
+    if not word:
+        return False
+    if  high_index == low_index:
+        return True
+    if (high_index - low_index) == 1 and word[low_index] == word[high_index]:
+        return True
+    if word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    else:
         return False
 
 
-print(is_palindrome_recursive('aggua', 1, 2))
