13163721c24a78642ed71d23fe19c9e408dc8fd5
Laisa Rio Verde <laisa.rioverde@gmail.com>
2023-07-17 20:11:26 -0300

    last changes

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,6 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or isinstance(nums, str) or isinstance(nums, int):
+        return False
+    for number in nums:
+        if number < 0:
+            return False
