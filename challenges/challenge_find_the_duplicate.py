a135f64d6d6ca0b3d34f1bbd47ca8bfff53cd16d
Laisa Rio Verde <laisa.rioverde@gmail.com>
2023-07-18 10:46:36 -0300

    fineshid project

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,6 +1,11 @@
 def find_duplicate(nums):
-    if not nums or isinstance(nums, str) or isinstance(nums, int):
+    if not nums or isinstance(nums, int) or len(nums) < 2:
         return False
-    for number in nums:
-        if number < 0:
+    n = len(nums)
+    ordered_nums = sorted(nums)
+    for number in range(0, n - 1):
+        if isinstance(ordered_nums[number], str) or ordered_nums[number] < 1:
             return False
+        if ordered_nums[number] == ordered_nums[number + 1]:
+            return ordered_nums[number]
+    return False
